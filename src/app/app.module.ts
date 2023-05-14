859289ed6fc61a66b16c6e19ff4ffdf70022a7f4
Dennis Winter <dennis.winter@mail.de>
2023-05-14 16:06:14 +0200

    ng g c ball

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {BallComponent} from './ball/ball.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BallComponent
   ],
   imports: [
     BrowserModule,
